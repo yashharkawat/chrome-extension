2dd65dfbca64a39789c28e9c82c73210fbbadd81
yash harkawat <yashharkawat2000@gmail.com>
2022-06-27 17:43:27 +0530

    code changes

--- content.js
+++ content.js
@@ -1,143 +1,89 @@
-// import uniqolor from 'uniqolor/src/index.js';
-// const uniqolor = require('uniqolor');
-console.log("content scipt working");
-function getRandomColor() {
-    var letters = '0123456789ABCDEF'.split('');
+const getRandomHexValue=()=> {
+    const letters = '0123456789ABCDEF'.split('');
     var color = '#';
     for (var i = 0; i < 6; i++ ) {
         color += letters[Math.floor(Math.random() * 15)];
     }
     return color;
 }
-const high=(name,node)=>{
-    const range = new Range();
-    const text=node.textContent;
-    range.setStart(node,0);
-    range.setEnd(node,text.length);
-    const highlight = new Highlight(range);
-    CSS.highlights.set('my-custom-highlight', highlight);
-}
-const helperFun=(string)=>{
-    let n=string.length;
-    let str="";
-    for(let i=0;i<n;i++)
+const getTargetStringRegex=(string)=>{
+    let newString="";
+    for(let i=0;i<string.length;i++)
     {
         if(string[i]==='.'){
-            str+="\\";
+            newString+="\\";
         }
-        str+=string[i];
+        newString+=string[i];
     }
     let k=-1;
-    let arr=[];
-    for(let i=1;i<str.length-1;i++)
+    let indexArray=[];
+    for(let i=1;i<newString.length-1;i++)
     {
-        if(str[i]==='{'&&str[i-1]==='{') k=i-1;
-        if(str[i]==='}'&&str[i+1]==='}')
+        if(newString[i]==='{'&&newString[i-1]==='{') k=i-1;
+        if(newString[i]==='}'&&newString[i+1]==='}')
         {
             if(k>=0) 
             {
-                arr.push([k,i+1]);
+                indexArray.push([k,i+1]);
                 k=-1;        
             } 
         }
 
     }
-    return [str,arr];
-}
-//returns all string which has the same pattern as the corresponding string. 
-const patternMatch=(text,string)=>{
-    let [str,arr]=helperFun(string);
-    let newStr="";
-    let k=0;
+    let RegexString="";
+    k=0;
 
-    arr.forEach((item)=>{
-        newStr+=str.slice(k,item[0]);
-        newStr+="\\w*";
+    indexArray.forEach((item)=>{
+        RegexString+=newString.slice(k,item[0]);
+        RegexString+="\\w*";
         k=item[1]+1;
     })
-    newStr+=str.slice(k,str.length);
-    
-    let regEx=new RegExp(`${newStr}`);
-    let array=[];
+    RegexString+=newString.slice(k,newString.length);
+    return RegexString;
+}
+
+const matchLabelsWithString=(text,string)=>{
+    const regexString=getTargetStringRegex(string);
+    let regEx=new RegExp(`${regexString}`);
 
     return regEx.test(text);
 }
-//helper function to loop through all nodes.
-const dfs = (node,arrayOfNodes) => {
+
+const dfs = (node,DocumentNodes) => {
   if(node===null) return;
   if(node.nodeName==='STYLE') return;
 
-  arrayOfNodes.push(node);
+  if(node.nodeType===Node.TEXT_NODE) DocumentNodes.push(node);
   node.childNodes.forEach((item) => {
-      dfs(item,arrayOfNodes);
+      dfs(item,DocumentNodes);
   });
-  return arrayOfNodes;
+  return DocumentNodes;
 };
 
-const func=(object,name)=>{
-    //const name=name;
-    for(let key in object)
-    {
-        const str=object[key];
-        const arrayOfNodes=dfs(document.body,[]);
-        arrayOfNodes.forEach((item)=>{
-            if(item.nodeType===Node.TEXT_NODE)
+const HighlightNodes=(object,DocumentNodes)=>{
+    const name=Object.keys(object)[0];
+    const i18nStrings=object[name];
+    i18nStrings.forEach((string)=>{
+        DocumentNodes.forEach((item)=>{
+            const text=item.textContent;
+            if(matchLabelsWithString(text,string))
             {
-                
-                const text=item.textContent;
-                //console.log(text);
-                
-
-                if(patternMatch(text,str))
-                {
-                    // const range = new Range();
-                    // range.setStart(item,0);
-                    // range.setEnd(item,text.length);
-                    // const highlight = new Highlight(range);
-                    // CSS.highlights.set('my-custom-highlight', highlight);
-                    // document.getSelection().removeAllRanges();
-                    // document.getSelection().addRange(range);/
-                    const newElement=document.createElement("mark");
-                    
-                    newElement.classList.add(name);
-                    newElement.innerHTML=text;
-                    const parent=item.parentNode;
-                    parent.replaceChild(newElement,item);
-                }
+                const newElement=document.createElement("mark");
+                newElement.classList.add(name);
+                newElement.innerHTML=text;
+                const parent=item.parentNode;
+                parent.replaceChild(newElement,item);
             }
         })
-    }
-    const strr=`.${name}`;
-    //const st=JSON.stringify(strr);
-    const arr=document.querySelectorAll(strr);
-    var color=getRandomColor();
-    arr.forEach((item)=>{
-        //var color = uniqolor(name);
-        //var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    })
+    const markedNodes=document.querySelectorAll(`.${name}`);
+    const color=getRandomHexValue();
+    markedNodes.forEach((item)=>{
         item.style.backgroundColor=color;
     })
+    
 }
-
-const obj=
-{
-    "You have changed the language {{count}} times": "You have changed the language {{count}} times"
-}
-;
-const obj2=
-{
-    "welcome": "Welcome!"
-};
-// func(obj);
-// func(obj2);
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    func(message[0],message[1]);
-    //console.log(sender)
-    sendResponse("hi")
+chrome.runtime.onMessage.addListener((message) => {
+    const DocumentNodes=dfs(document.body,[]);
+    HighlightNodes(message,DocumentNodes);
 })
-
-// "content_scripts":[
-//     {
-//     "matches":["<all_urls>"],
-//     "js":["content.js"]
-// }   
-// ],
--- devtools.js
+++ devtools.js
@@ -1,119 +1,27 @@
-const con=chrome.devtools.inspectedWindow.eval;
-const display=(item)=>{
-  con('console.log(unescape("' +escape(item) + '"))');
-}
-
 chrome.devtools.network.onRequestFinished.addListener(request => {
     request.getContent((body) => {
       if (request.request && request.request.url) {
         const urlLink=request.request.url;
         const name=urlLink.split("/").pop();
-
         const regEx=new RegExp("\.json$");
+
         if(regEx.test(name)&&name!=="manifest.json")
         {
-          const newName=name.split('.');
-          const Name=newName[0];
-          // chrome.tabs.onActivated.addListener((tab) => {
-          //   display(JSON.stringify(tab));
-          //   chrome.tabs.get(tab.tabId, (CurrentTabData) => {
-          //     chrome.scripting.executeScript({
-          //     target: { tabId: CurrentTabData.id },
-          //     files: ['content.js']
-          //   });
-          //   })
-          //   setTimeout(()=>{
-          //     chrome.tabs.sendMessage(tab.tabId,JSON.parse(body),(response)=> { 
-          //       display(response)
-          //       })
-          //   },2000);
-          //   //display(name);
-          //   //display(body);
+          const nameOfFile=name.split('.')[0];    
+          const bodyObject=JSON.parse(body);
+          const i18nStrings=[];
           
-          // })
-          const array=[JSON.parse(body),Name];
-          chrome.tabs.query({currentWindow:true,active:true},(tabs) => {
-            var tab=tabs[0];
-            //display(JSON.stringify(tab));
-            setTimeout(()=>{
-              chrome.tabs.sendMessage(tab.id,array,(response)=> { 
-                display(response)
-                })
-            },2000);
-            chrome.tabs.get(tab.id, (CurrentTabData) => {
-              chrome.scripting.executeScript({
-              target: { tabId: CurrentTabData.id },
-              files: ['content.js']
-            });
-            })
-            
-            //display(name);
-            //display(body);
-          
+          for(let key in bodyObject){
+            i18nStrings.push(bodyObject[key]);
+          }
+          const responseObject={};
+          responseObject[nameOfFile]=i18nStrings;
+
+          chrome.tabs.query({currentWindow:true,active:true},([tab]) => {
+            chrome.tabs.sendMessage(tab.id,responseObject)
           })
-        }
-        
+        }  
         
       }
     });
   });
-
-//display(dict.size);
-  
-  // load
-  // chrome.storage.local.get( "mp",
-  //   function(saved){
-  //     const mp = new Map(Object.entries(result.mp));
-  //   }
-  // )
-  // chrome.storage.sync.get("map", ({ mp }) => {
-  //   con('console.log(unescape("' +escape(mp.size) + '"))');
-  // });
-//let ss=arr.size;
-//con('console.log(unescape("' +escape(ss) + '"))');
-// for(let [keys,value] of mp){  
-//   display(keys);
-//   display(value);   
-// }
-// display(mp.size);
-// display(mp.size+1);
-
-// if(regEx.test(name)&&name!=="manifest.json")
-//         {
-//           const obj=JSON.parse(body);
-
-//           chrome.storage.sync.get("object",({object})=>{
-//             const newObject=JSON.parse(JSON.stringify(object));
-//             let a=0;
-//             display(a);
-//             display(JSON.stringify(newObject));
-//             newObject[urlLink]=body;
-            
-            
-//             display(JSON.stringify(newObject));
-//             //display(JSON.stringify(newObject));
-//             display(a);
-//             // for(let key in newObject)
-//             // {
-//             //   a+=1;
-//             //   display(a);
-//             //   display(key);
-//             //   display(newObject[key]);
-//             // }
-            
-            
-//             chrome.storage.sync.set({"object":newObject});
-//             //display(object[b]);
-//           })
-//           //display(JSON.stringify(newObject));
-          
-//           chrome.storage.sync.get("object",({object})=>{
-//             display("object  is");
-//             display(JSON.stringify(object));
-//           })
-//           // for(let key in obj)
-//           // {
-//           //   dict.push(obj[key]);
-//           // }
-//           //display(obj);
-//         }
